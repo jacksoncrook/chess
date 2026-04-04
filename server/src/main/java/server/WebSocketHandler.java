a813963181ec2d18c04aad871d08c0db0b7f9722
Jackson Crook <jacksoncrook01@gmail.com>
2026-04-04 16:03:22 -0600

    Added support for resignation and different types of databases in WebSocketHandler.java; Also added proper notifications to users

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -1,6 +1,7 @@
 package server;
 
 import com.google.gson.Gson;
+import dataaccess.DataAccessException;
 import io.javalin.websocket.WsCloseContext;
 import io.javalin.websocket.WsCloseHandler;
 import io.javalin.websocket.WsConnectContext;
@@ -9,14 +10,25 @@ import io.javalin.websocket.WsMessageContext;
 import io.javalin.websocket.WsMessageHandler;
 import org.eclipse.jetty.websocket.api.Session;
 import org.jetbrains.annotations.NotNull;
+import service.GameService;
+import service.UserService;
 import websocket.commands.UserGameCommand;
-import websocket.messages.ServerMessage;
+import websocket.commands.UserResignCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.Notification;
 
 import java.io.IOException;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
     private final ConnectionManager connections = new ConnectionManager();
+    private final GameService gameService;
+    private final UserService userService;
+
+    public WebSocketHandler(String databaseType) throws DataAccessException {
+        gameService = new GameService(databaseType);
+        userService = new UserService(databaseType);
+    }
 
     @Override
     public void handleConnect(WsConnectContext ctx) {
@@ -28,11 +40,13 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
     public void handleMessage(WsMessageContext ctx) {
         try {
             UserGameCommand userGameCommand = new Gson().fromJson(ctx.message(), UserGameCommand.class);
+            String username = userService.getUsername(userGameCommand.getAuthToken());
             switch (userGameCommand.getCommandType()) {
-                case CONNECT -> connect(userGameCommand.getGameID(), ctx.session);
-                case LEAVE -> leave(userGameCommand.getGameID(), ctx.session);
+                case CONNECT -> connect(userGameCommand.getGameID(), username, ctx.session);
+                case LEAVE -> leave(userGameCommand.getGameID(), username, ctx.session);
+                case RESIGN -> resign(new Gson().fromJson(ctx.message(), UserResignCommand.class), username, ctx.session);
             }
-        } catch (IOException ex) {
+        } catch (IOException | DataAccessException ex) {
             ex.printStackTrace();
         }
     }
@@ -42,15 +56,26 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         System.out.println("Websocket closed");
     }
 
-    private void connect(Integer gameID, Session session) throws IOException {
+    private void connect(Integer gameID, String username, Session session) throws IOException {
         connections.add(gameID, session);
-        var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+        var notification = new Notification(username + " has joined the game!");
         connections.broadcast(gameID, session, notification);
     }
 
-    private void leave(Integer gameID, Session session) throws IOException {
-        var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+    private void leave(Integer gameID, String username, Session session) throws IOException {
+        var notification = new Notification(username + " has left the game!");
         connections.broadcast(gameID, session, notification);
         connections.remove(gameID, session);
+    }
+
+    private void resign(UserResignCommand command, String username, Session session) throws IOException {
+        try {
+            gameService.resignGame(command.getAuthToken(), command.getGameID(), command.teamColor);
+        } catch (DataAccessException e) {
+            connections.sendMsg(session, new ErrorMessage(e.getMessage()));
+            return;
+        }
+        var notification = new Notification(username + ": " + command.teamColor + " has resigned!");
+        connections.broadcast(command.getGameID(), null, notification);
     }
 }
